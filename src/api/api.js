c2418171b39f883d19a72c5df1c2e55b442d402d
Fas Al Yashin <fasyashin@gmail.com>
2026-09-24 09:18:10 +0600

    Update API base URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+   baseURL: "https://bloodbridge-backend-t3yu.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
